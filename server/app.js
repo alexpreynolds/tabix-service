cf07e255d73fcff36887f5243aadba20bffc9b7b
Denise Mauldin <dmauldin@altius.org>
2017-06-01 21:53:31 -0700

    Updated to use CORS to handle headers.  Added a /chromosomeList to return all chromosomes in the archive.

--- server/app.js
+++ server/app.js
@@ -4,12 +4,13 @@ var TabixServer = TabixServer || {};
 
 var express = require('express');
 var request = require('request');
+var cors = require('cors');
 var url = require('url');
 var valid_url = require('valid-url');
 var fs = require('fs');
 var path = require('path');
 var winston = require('winston');
-var child_process = require('child_process');
+var spawn = require('child_process').spawnSync;
 
 TabixServer.App = express();
 
@@ -18,86 +19,91 @@ TabixServer.DefaultPort = 1234;
 TabixServer.CurrentDir = process.cwd();
 
 TabixServer.DatasetTypes = {
-    SAMPLE : 'sample'
+  SAMPLE: 'sample'
 };
 
 TabixServer.DatasetPaths = {
-    SAMPLE : 'archives/sample.bed.gz'
+  SAMPLE : 'archives/sample.bed.gz'
 };
 
-TabixServer.Logger = new (winston.Logger)({
-    transports: [
-	new (winston.transports.Console)({
-	    json: false,
-	    colorize: true,
-	    timestamp: true
-	})
-    ]
+TabixServer.Logger = new(winston.Logger)({
+  transports: [
+    new(winston.transports.Console)({
+      json: false,
+      colorize: true,
+      timestamp: true
+    })
+  ]
 });
 
 TabixServer.NormalizePort = function(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-    return false;
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
 };
 
-TabixServer.App.use('/', function(req, res, next) {
-    TabixServer.Logger.info(`request url: ${req.url}`);
-    var requestCorrect = true;
-    if (req.method == 'GET') {
-	var query = url.parse(req.url, true).query;
-	var queryStr = JSON.stringify(query);
-	TabixServer.Logger.info(`request query: ${queryStr}`);
-	if (query.dataset) {
-	    if (query.dataset == TabixServer.DatasetTypes.SAMPLE) {
-		if (query.chr && query.start && query.stop) {
-		    res.writeHead(200, {
-			"Content-Type": "text/plain",
-			"Cache-control": "no-cache",
-			"Access-Control-Allow-Origin": "*",
-		    });
-		    var tabixArchive = path.join(TabixServer.CurrentDir, TabixServer.DatasetPaths.SAMPLE);
-		    var tabixROI = query.chr + ":" + query.start + "-" + query.stop;
-		    var tabixQuery = child_process.spawn('tabix', [tabixArchive, tabixROI]);
-		    var tabixResult = "";
+TabixServer.App.use(cors())
+TabixServer.App.get('/', function(req, res, next) {
+  TabixServer.Logger.info(`request url: ${req.url} ${req.method}`);
+  var requestCorrect = true;
+  if (req.method == 'GET') {
+    TabixServer.Logger.info("get returning query results");
+    var query = url.parse(req.url, true).query;
+    if (query.dataset) {
+      TabixServer.Logger.info("query.dataset is set");
+      if (query.dataset == TabixServer.DatasetTypes.SAMPLE) {
+        TabixServer.Logger.info("query.dataset is sample");
+        if (query.chr && query.start && query.stop) {
+          TabixServer.Logger.info("query.dataset has chr, start, stop");
+          var tabixArchive = path.join(TabixServer.CurrentDir, TabixServer.DatasetPaths.SAMPLE);
+          var tabixROI = query.chr + ":" + query.start + "-" + query.stop;
+          var child = spawn('tabix', [tabixArchive, tabixROI]);
+          var tabixResult = child.stdout.toString().split("\n");
+          tabixResult.pop();
+          tabixResult = tabixResult.map((a) => a.split("\t"));
+          res.write(JSON.stringify(tabixResult));
+          res.end();
+        } else {
+          requestCorrect = false;
+        }
+      } else {
+        res.sendStatus(404);
+        return;
+      }
+    } else {
+       requestCorrect = false;
+    }
+  } // if req.method == GET
+  else {
+    requestCorrect = false;
+  }
+  if (!requestCorrect) {
+    res.sendStatus(400);
+  }
+});
 
-		    tabixQuery.stdout.pipe(res);
-
-		    tabixQuery.stderr.on('data', function(data) {
-			TabixServer.Logger.error(`${data}`);
-			res.end('stderr: ' + data);
-		    });
-		}
-		else {
-		    requestCorrect = false;
-		}
-	    }
-	    else {
-		res.sendStatus(404);
-		return;
-	    }
-	}
-	else {
-	    requestCorrect = false;
-	}
-    }
-    else {
-	requestCorrect = false;
-    }
-    if (!requestCorrect) {
-	res.sendStatus(400);
-    }
+TabixServer.App.get('/chromosomeList', function(req, res, next) {
+  TabixServer.Logger.info(`query.chromosomeList`);
+  var tabixArchive = path.join(TabixServer.CurrentDir, TabixServer.DatasetPaths.SAMPLE);
+  var child = spawn('tabix', ['-l', tabixArchive]);
+  var tabixResult = child.stdout.toString().split("\n");
+  tabixResult.pop();
+  tabixResult = tabixResult.sort((a, b) => a.localeCompare(b, 'en', {
+    numeric: true
+  }));
+  res.write(JSON.stringify(tabixResult));
+  res.end();
 });
 
 var http_port = TabixServer.NormalizePort(process.env.TABIX_SERVER_PORT || TabixServer.DefaultPort);
 
 var http_server = TabixServer.App.listen(http_port, function() {
-    TabixServer.Logger.info(`TabixServer is listening on port ${http_port}`)
+  TabixServer.Logger.info(`TabixServer is listening on port ${http_port}`)
 });
